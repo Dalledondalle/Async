dc537ced81df849952b4c10f784ae2113d9f038e
Daniel Kåre Kristensen <dani9241@elev.techcollege.dk>
2020-11-17 21:15:33 +0100

    Add project files.

---
+++ Async/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static readonly Random s_rnd = new Random();
+
+    static async Task Main()
+    {
+        Console.WriteLine($"You rolled {await GetDiceRollAsync()}");
+    }
+
+
+    static async ValueTask<int> GetDiceRollAsync()
+    {
+        Console.WriteLine("Shaking dice...");
+
+        int roll1 = await RollAsync();
+        int roll2 = await RollAsync();
+        Console.WriteLine("Should be 2nd");
+
+        return roll1 + roll2;
+    }
+
+    static async ValueTask<int> RollAsync()
+    {
+        await Task.Delay(500);
+
+        int diceRoll = s_rnd.Next(1, 7);
+        return diceRoll;
+    }
+}
